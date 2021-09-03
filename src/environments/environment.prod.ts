de1e92b529c8f6cf15b7d1662c48c44bc3f194bf
HDONG <377749229@qq.com>
2021-09-03 17:14:05 +0800

    修改env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,34 @@
-import { Environment } from '@delon/theme';
+const baseUrl = 'http://localhost:4200';
+const authUrl = 'https://localhost:44386';
+const serviceUrl = 'https://localhost:44342';
 
 export const environment = {
-  production: true,
+  production: false,
   useHash: true,
+  localization: {
+    defaultResourceName: 'Blog',
+  },
   api: {
     baseUrl: './',
     refreshTokenEnabled: true,
     refreshTokenType: 'auth-refresh',
   },
-} as Environment;
+  application: {
+    baseUrl,
+    name: 'Blog',
+    logoUrl: '/assets/logo.png',
+  },
+  oAuthConfig: {
+    issuer: authUrl,
+    redirectUri: baseUrl,
+    clientId: 'Blog_App',
+    responseType: 'code',
+    scope: 'offline_access openid profile role email phone Blog',
+  },
+  apis: {
+    default: {
+      url: serviceUrl,
+      rootNamespace: 'Bcvp.Blog',
+    },
+  },
+};
